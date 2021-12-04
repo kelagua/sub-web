2b6090935903fa9b138c94ad80f4502d52e8ffdb
kelagua <50149727+kelagua@users.noreply.github.com>
2021-12-05 05:39:46 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "yellow-waterfall-ff0b.1193545969.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
